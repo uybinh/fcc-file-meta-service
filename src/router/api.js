e89cf34965e1b3363c111dbf163187676a0fddb1
Uy Binh <uybinh@gmail.com>
2018-10-16 21:45:59 +0700

    Add route

--- src/router/api.js
+++ src/router/api.js
@@ -1,5 +1,16 @@
 const express = require("express")
 const router = express.Router()
+const formidable = require("formidable")
 require("../db/database")
+
+router.post("/upload", (req, res) => {
+	const form = new formidable.IncomingForm()
+	form.maxFileSize = 10 * 1024 * 1024
+	form.parse(req, function(err, fields, files) {
+		if (err) return res.json(err)
+		const { name, type, size } = files.upfile
+		res.json({ name, type, size })
+	})
+})
 
 module.exports = router
